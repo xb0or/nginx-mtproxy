4b5d0b2ad4659638f178ed9a6759ad02a307118a
xb0or <57210034+xb0or@users.noreply.github.com>
2021-06-26 12:45:59 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,8 +12,7 @@ install_docker() {
     systemctl enable docker  
 }
 install_docker_compose() {
-        curl -L "https://cdn.jsdelivr.net/gh/docker/compose@releases/download//1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	#curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 }
 
@@ -46,7 +45,9 @@ check_docker_compose() {
 #开始菜单
 start_menu(){
     clear
-    echo -e "\033[0;33m  确认请按回车键Enter；否则按Ctrl+C退出 \033[0m"
+    echo -e "\033[32m====================================\033[0m"
+    echo -e "\033[0;33m  即将开始安装Docker；否则按Ctrl+C退出 \033[0m"
+    echo -e "\033[32m====================================\033[0m"
     echo
     read -p "是否继续？:" num
     case "$num" in   
