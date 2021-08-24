99e02ff3d25dff800e7bf9fe2e421673df625c87
xb0or <57210034+xb0or@users.noreply.github.com>
2021-08-24 19:09:02 +0800

    Update mtp.sh

--- mtp.sh
+++ mtp.sh
@@ -52,5 +52,5 @@ docker update --restart=always nginx-mtproxy
     echo -e "服务器IP：\033[31m$public_ip\033[0m"
     echo -e "服务器端口：\033[31m$port\033[0m"
     echo -e "MTProxy Secret:  \033[31m$client_secret\033[0m"
-    echo -e "白名单认真地址：https://${public_ip}/add.php"
+    echo -e "TG认证地址：https://${public_ip}/add.php"
     echo -e "TG一键链接: https://t.me/proxy?server=${public_ip}&port=${port}&secret=${client_secret}"
